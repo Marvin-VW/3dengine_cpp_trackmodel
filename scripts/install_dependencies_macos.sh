6f6ad74151e7ad78d38a4fd07145a52e4f9bddf0
Lorenz <marvin.rocket.64@gmail.com>
2024-09-27 11:19:23 +0200

    added cicd tests for macos

---
+++ scripts/install_dependencies_macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed"
+fi
+
+brew update
+
+brew install cmake
+brew install opencv
+brew install git
+
+echo "Dependencies installed successfully."
